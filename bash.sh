b3374259e30c5451600119b349c82f4e0d55fc46
LorenzoLaur <118292225+LorenzoLaur@users.noreply.github.com>
2022-12-06 23:26:42 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,7 +11,7 @@ function SystemUsage(){
     DISK: $disk"
 }
 # Checking if something has been passed on, if it's not then it will run the if statement 
-if [$# -eq 0]; then
+if [ $# -eq 0]; then
     echo "Usage: $0 [command]"
     echo "Commands:"
     echo "  help - print this help message"
@@ -19,7 +19,7 @@ if [$# -eq 0]; then
 fi
 
 # Checking if something has been passed on, if "help" has been passed it will run the if statement 
-if [$1 = "help"]; then
+if [ $1 = "help"]; then
     echo -e "   add-user - create a user"
     echo -e "   firewall - add or remove a firewall port"
     echo -e "   update-system - update system"
@@ -29,7 +29,7 @@ if [$1 = "help"]; then
 fi
 
 # Checking if something has been passed on, if "system-update" has been passed it will run the if statement
-if [$1 = "system-update"]; then
+if [ $1 = "system-update"]; then
     echo -e "updating system"
     sudo apt-get update && sudo-apt get upgrade -y
     wait
@@ -38,27 +38,27 @@ if [$1 = "system-update"]; then
 fi
 
 # Checking if something has been passed on, if "firewall" has been passed it will run the if statement
-if [$1 = "firewall"]; then
+if [ $1 = "firewall"]; then
     # Checking if 2nd var is passed to firewall, if not echo error
-    if [-z "$2"]; then
+    if [ -z "$2" ]; then
         echo -e "Must specify if you want to [add] or [remove] a port"
         exit 0
     fi
 
     # Checking if 3rd var is passed to firewall, if not echo error
-    if [-z "$3"]; then
+    if [ -z "$3" ]; then
         echo -e "Must include a port"
         exit 0
     fi
 
-    if [$2 = "add"]; then
+    if [ $2 = "add"]; then
         sudo ufw allow $3
         wait
         echo -e "Port $3 added to firewall"
         exit 0
     fi
 
-    if [$2 = "remove"]; then
+    if [ $2 = "remove"]; then
         sudo ufw delete allow $3
         wait
         echo -e "Port $3 removed from firwall"
@@ -67,13 +67,13 @@ if [$1 = "firewall"]; then
 fi
 
 # Checking if something has been passed on, if "system-stats" has been passed it will run the if statement and call the function declared at the beggining
-if [$1 = "system-stats"]; then
+if [ $1 = "system-stats"]; then
     SystemUsage
     wait 
     exit 0
 fi
 # Checking if something has been passed on, if "setup-wp" has been passed it will run the if statement
-if [$1 = "setup-wp"]; then
+if [ $1 = "setup-wp"]; then
     # Installing Nginx
     sudo-apt install nginx -y
     wait
@@ -160,7 +160,7 @@ if [$1 = "setup-wp"]; then
     exit 0
 fi
 # Checking if something has been passed on, if "create-user" has been passed it will run the if statement
-if [$1 = "create-user"]; then
+if [ $1 = "create-user"]; then
     echo -e "** MAKE SURE TO KEEP A RECORD OF THE USERS CREDENTIALS**"
     # Creating a new user 
     sudo adduser newuser
