46b834df6f5b4ed99558c3f82ce2646ecedef9c9
LorenzoLaur <118292225+LorenzoLaur@users.noreply.github.com>
2022-12-06 13:11:26 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,3 +3,10 @@ if [ $# -eq 0  ]; then
     echo -e "Commands:";
     echo -e "  help - gives list of commands you can run";
 fi
+
+if [ $1 = "help" ]; then
+    echo -e "  add-user - create a user";
+    echo -e "  firewall - add or remove a firewall port"; 
+    echo -e "  update-system - update system";
+    echo -e "   setup-wp - installs apts neeeded for wp";
+fi
